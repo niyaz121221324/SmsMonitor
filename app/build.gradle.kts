74560ef1d9eb391ad41ec8b98cd7abc4ab20159f
niazananev698 <niazananev698@gmail.com>
2024-11-30 23:23:04 +0500

    fix[appInsightsSettings.xml, build.gradle.kts, libs.versions.toml]: update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,6 +65,7 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.androidx.annotation)
     implementation(libs.cronet.embedded)
+    implementation(libs.androidx.activity.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
